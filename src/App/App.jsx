55c827167dab898a23223428d98914ee1f678c4a
Veronica Andrade <vandrade@invoca.com>
2022-03-08 08:10:11 -0700

    Update current file structure

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+    render() {
+        return (
+            <h1>Flixter</h1>
+        )
+    }
+}
+
+export default App;
